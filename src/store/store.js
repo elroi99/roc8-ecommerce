7c873220b7ce06b1a718b8916f427c9df1a7b386
elroi noronha <elroinoronha2@gmail.com>
2023-03-26 11:06:50 +0530

    initial commit

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { useReducer } from "react";
+
+const initialState = {
+  products: [],
+  cartProducts: [],
+  isLoading: [],
+  isError: [],
+  isOffline: [],
+};
+
+function useStore() {}
